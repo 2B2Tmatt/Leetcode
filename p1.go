5ffd1bc81163e86fb3016998faa65d9aea02583f
Matt Ge <mattyige@gmail.com>
2026-01-11 19:47:10 -0500

    1/11 map solution to two sum

--- p1.go
+++ p1.go
@@ -1,33 +1,16 @@
 package main
 
-import (
-	"slices"
-)
-
-type numVal struct {
-	val int
-	idx int
-}
-
 func twoSum(nums []int, target int) []int {
-	newNums := make([]numVal, 0)
-	for i, num := range nums {
-		newNums = append(newNums, numVal{val: num, idx: i})
-	}
-	slices.SortFunc(newNums, func(a, b numVal) int {
-		return a.val - b.val
-	})
 	answers := make([]int, 2)
-	p1, p2 := 0, len(nums)-1
-	for p1 <= p2 {
-		if newNums[p1].val+newNums[p2].val == target {
-			answers[0] = newNums[p1].idx
-			answers[1] = newNums[p2].idx
-			return answers
-		} else if newNums[p1].val+newNums[p2].val > target {
-			p2--
+	seen := make(map[int]int)
+	for i, num := range nums {
+		need := target - num
+		if idx, exists := seen[need]; exists {
+			answers[0] = i
+			answers[1] = idx
+			break
 		} else {
-			p1++
+			seen[num] = i
 		}
 	}
 	return answers
